ad9da59ccb001b97b42155f28f8a67c9ad63a099
mcmahj45 <Jack.mcmahon45@mail.dcu.ie>
2026-03-11 15:57:21 +0000

    adding starter code

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="">
+      <div className="">
+        <h1 className="text-5xl">
+          Jack McMahon.
+        </h1>
+        <p className="text-xl">
+          Computer science student
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
