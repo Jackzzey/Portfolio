42da98ae386aebfaa4d5857fbd0dd26218c5d61b
mcmahj45 <Jack.mcmahon45@mail.dcu.ie>
2026-03-30 15:32:07 +0100

    fixing tailwindcss

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,28 +1,8 @@
-import Navbar from '../components/Navbar';
-
 function Home() {
   return (
-    <div
-      className="bg-cover bg-center bg-red h-screenflex items-center justify-center"
-      style={{ backgroundImage: "url('/images/IMG_20210810_143335.jpg')" }}
-    >
-      <div className="bg-black/50 p-6 rounded text-center text-white">
-        <h1 className="text-5xl font-bold">Jack McMahon</h1>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-        <p className="text-xl mt-2">Computer Science Student</p>
-      </div>
-    </div>
+    <div className="bg-red-500 text-white min-h-screen flex items-center justify-center">
+  TEST
+</div>
   );
 }
 
