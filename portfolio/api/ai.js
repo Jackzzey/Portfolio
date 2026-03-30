542b424a348cc459a0d479488202f9e9ea17eb94
mcmahj45 <Jack.mcmahon45@mail.dcu.ie>
2026-03-30 16:05:38 +0100

    fixing elint config file and adding it back

--- portfolio/api/ai.js
+++ portfolio/api/ai.js
@@ -17,11 +17,6 @@ export default async function handler(req, res) {
     rateLimit.set([]);
 
   }
-<<<<<<< HEAD
-=======
-
-const timestamps = rateLimit.get(ip);
->>>>>>> 3b57272533b0eb80b60aa942377317047da17c31
 
   const timestamps = rateLimit.get(ip);
 
